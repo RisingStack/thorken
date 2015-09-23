178140d2a0ea839277ec69bf738bbc2527f94544
Peter Marton <peter@risingstack.com>
2015-09-23 16:12:18 +0200

    chore(package): initial commit

---
+++ src/testSetup.spec.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var sinon = require('sinon')
+var chai = require('chai')
+var sinonChai = require('sinon-chai')
+var debug = require('debug')
+
+debug.enable('token-session:*')
+
+before(function () {
+  chai.use(sinonChai)
+
+  sinon.stub.returnsWithResolve = function (data) {
+    return this.returns(Promise.resolve(data))
+  }
+
+  sinon.stub.returnsWithReject = function (error) {
+    return this.returns(Promise.reject(error))
+  }
+})
+
+beforeEach(function () {
+  this.sandbox = sinon.sandbox.create()
+})
+
+afterEach(function () {
+  this.sandbox.restore()
+})
